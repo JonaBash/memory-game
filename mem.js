67ffae05be2881a8cd12f5ab64ed089c918a6274
Jona <bashanyo@gmail.com>
2018-11-19 19:39:34 +0200

    made responsive, scoped inside objects all global vars, took out all logs, fixed all board bugs like clicking on pairs that have been found already

--- mem.js
+++ mem.js
@@ -9,54 +9,56 @@ memory.game = function () {
     memory.card = $('.box');
     memory.card.on('click', memory.choose);
 }
-var clickCounter = 0;
-var winCounter = 0;
-var firstCheck;
-var secondCheck;
-var arrCheckFirst;
-var arrCheckSecond;
-var fc, sc;
+memory.clickCounter = 0;
+memory.winCounter = 0;
+memory.firstCheck;
+memory.secondCheck;
+memory.arrCheckFirst;
+memory.arrCheckSecond;
+memory.fc; 
+memory.sc;
 
 memory.choose = function () {
     this.classList.toggle('flip');
-    clickCounter++;
+    memory.clickCounter++;
 
 
-    if (clickCounter == 1) {
-        fc = this;
+    if (memory.clickCounter == 1) {
+        memory.fc = this;
         $(this).css('pointer-events', 'none');
-        firstCheck = $(this).attr('id');
-        arrCheckFirst = firstCheck.substring(2);
-        console.log(arrCheckFirst);
+        memory.firstCheck = $(this).attr('id');
+        memory.arrCheckFirst = memory.firstCheck.substring(2);
     }
-    if (clickCounter == 2) {
-        $(fc).removeAttr('style', 'pointer-events');
-        sc = this;
+    if (memory.clickCounter == 2) {
+        memory.sc = this;
         $('.container').css('pointer-events', 'none');
-        secondCheck = $(this).attr('id');
-        arrCheckSecond = secondCheck.substring(2);
-        console.log(arrCheckSecond);
-        if (cards[arrCheckFirst] === cards[arrCheckSecond]) {
-            winCounter++;
-            fc.classList.add('two');
-            sc.classList.add('two');
-            console.log(winCounter);
+        $(memory.sc).css('pointer-events', 'none');
+        memory.secondCheck = $(this).attr('id');
+        memory.arrCheckSecond = memory.secondCheck.substring(2);
+        if (memory.cards[memory.arrCheckFirst] === memory.cards[memory.arrCheckSecond]) {
+            memory.winCounter++;
+            memory.fc.classList.add('two');
+            memory.sc.classList.add('two');
+        } 
+        else {
+            $(memory.fc).removeAttr('style', 'pointer-events');
+            $(memory.sc).removeAttr('style', 'pointer-events');
         }
 
         setTimeout(function () {
             $('.container').removeAttr('style', 'pointer-events');
-            fc.classList.remove('flip');
-            sc.classList.remove('flip');
+            memory.fc.classList.remove('flip');
+            memory.sc.classList.remove('flip');
         }, 1000);
-        clickCounter = 0;
+        memory.clickCounter = 0;
     }
 
-    if (winCounter == cards.length / 2) {
+    if (memory.winCounter == memory.cards.length / 2) {
         $('.container').css('display', 'none');
         $("body").append("<h1>You won!!!!</h1>");
     }
 }
-var cards = [
+memory.cards = [
     "./img/hanzo.png",
     "./img/junkrat.png",
     "./img/genji.png",
@@ -76,17 +78,17 @@ var cards = [
     "./img/mccree.png",
     "./img/zarya.png"
 ];
-function shuffle(a) {
+function shuffle(e) {
     var j, x, i;
-    for (i = a.length - 1; i >= 0; i--) {
+    for (i = e.length - 1; i >= 0; i--) {
         j = Math.floor(Math.random() * (i));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+        x = e[i];
+        e[i] = e[j];
+        e[j] = x;
     }
-    return a;
+    return e;
 }
-shuffle(cards);
-for (var i = 0; i < cards.length; i++) {
-    $('#card' + (i + 1)).attr('src', cards[i]);
+shuffle(memory.cards);
+for (var i = 0; i < memory.cards.length; i++) {
+    $('#card' + (i + 1)).attr('src', memory.cards[i]);
 }
